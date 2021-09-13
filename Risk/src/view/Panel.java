d17ad39fb3e8c8a91a58a9c940dfd4f2dd6d2b8a
jchaves1406 <jesus_dn_@hotmail.com>
2021-09-13 18:21:07 -0500

    Muestro mapa en pantalla

--- Risk/src/view/Panel.java
+++ Risk/src/view/Panel.java
@@ -1,10 +1,25 @@
 package view;
 
+import javax.swing.ImageIcon;
 import javax.swing.JPanel;
+
+
+import java.awt.Graphics;
+
 
 public class Panel extends JPanel{
     
     public Panel(){
         this.setVisible(true);
+    }
+
+
+    @Override
+    public void paint(Graphics graph) {
+        
+        ImageIcon background = new ImageIcon(getClass().getResource("../pics/mapa.jpeg"));
+        graph.clearRect(0, 0, 1200, 700);
+        graph.drawImage(background.getImage(), 0, 0, getWidth(), getHeight(), this);
+        
     }
 }
--- Risk/src/view/Window.java
+++ Risk/src/view/Window.java
@@ -2,6 +2,7 @@ package view;
 
 import java.awt.Color;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 
 import control.Controller;
@@ -9,19 +10,27 @@ import control.Controller;
 public class Window extends JFrame{
     private Controller control;
     private Panel panel = new Panel();
+    private JButton play = new JButton("Play");
 
     public Window(){
         this.setBounds(10, 10, 1500, 800);
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.setLayout(null);
         this.initComponents();
+        this.setExtendedState(6);
         this.setVisible(true);
     }
 
     public void initComponents() {
-        this.panel.setBounds(50, 100, 1200, 700);
+        this.play.setBounds(1300, 100, 100, 50);
+        this.add(play);
+        this.panel.setBounds(0, 0, 1200, 700);
         this.setBackground(new Color(133, 193, 233));
         this.add(panel);
+    }
+
+    public void setController(Controller c){
+        this.control = c;
     }
 
     public Panel getPanel(){
